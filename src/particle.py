7bd913a0fa77fb48a0b0211226cc2ef56c729f7e
Richard Anslow <r.anslow@outlook.com>
2022-06-03 21:06:21 +0100

    added pytests for particle module

--- src/particle.py
+++ src/particle.py
@@ -8,8 +8,8 @@ import numpy as np
 class Particle(object):
     def __init__(self, mass, position, velocity):
         self.mass = mass
-        self.__position = position
-        self.__velocity = velocity
+        self.position = position
+        self.velocity = velocity
 
         # split up position
         self.x_pos = position[0]
@@ -22,14 +22,14 @@ class Particle(object):
         self.z_vel = velocity[2]
 
     def __hash__(self):
-        pos_str = ",".join([str(pos) for pos in self.__position])
-        vel_str = ",".join([str(vel) for vel in self.__velocity])
+        pos_str = ",".join([str(pos) for pos in self.position])
+        vel_str = ",".join([str(vel) for vel in self.velocity])
 
         return hash(".".join([str(self.mass), pos_str, vel_str]))
 
     @property
     def position(self):
-        return self.__position
+        return self.position
 
     @position.setter
     def position(self, position):
@@ -40,23 +40,23 @@ class Particle(object):
                 self.x_pos = position[0]
                 self.y_pos = position[1]
                 self.z_pos = position[2]
-                self.__position = position
+                self.position = position
 
             else:
                 raise TypeError(
                     "The position vector \
-                    must be a numpy array of length 3."
+must be a numpy array of length 3."
                 )
 
         else:
             raise TypeError(
                 "The position vector must \
-                be a numpy array of length 3."
+be a numpy array of length 3."
             )
 
     @property
     def velocity(self):
-        return self.__velocity
+        return self.velocity
 
     @velocity.setter
     def velocity(self, velocity):
@@ -67,16 +67,16 @@ class Particle(object):
                 self.x_vel = velocity[0]
                 self.y_vel = velocity[1]
                 self.z_vel = velocity[2]
-                self.__velocity = velocity
+                self.velocity = velocity
 
             else:
                 raise TypeError(
                     "The velocity vector must \
-                    be a numpy array of length 3."
+be a numpy array of length 3."
                 )
 
         else:
             raise TypeError(
                 "The velocity vector must \
-                be a numpy array of length 3."
+be a numpy array of length 3."
             )
--- tests/test_particle.py
+++ tests/test_particle.py
@@ -1,45 +1,32 @@
-# import numpy as np
-# from src.particle import Particle
-# from src.particles import Particles
-
-# particles = Particles()
-
-# print(particles.__dict__)
-
-# particles.add_particle(
-# 	Particle(
-# 		mass=1,
-# 		position=np.array([1,2,3]),
-# 		velocity=np.array([2,3,4])
-# 	)
-# )
-
-# print(particles.__dict__)
-
-# particles.add_particle(
-# 	Particle(
-# 		mass=1.5,
-# 		position=np.array([-1,2,3]),
-# 		velocity=np.array([-2,3,4])
-# 	)
-# )
-
-# print(particles.__dict__)
-
-# particle1 = Particle(
-# 		mass=1.5,
-# 		position=np.array([-1,2,3]),
-# 		velocity=np.array([-2,3,4])
-# )
-
-# particle2 = Particle(
-# 	mass=1.5,
-# 	position=np.array([-1,2.01,3]),
-# 	velocity=np.array([-2,3,4])
-# )
-
-# print(hash(particle1) == hash(particle2))
+from src.particle import Particle
+import numpy as np
+import pytest
 
 
 def test_particle():
-    assert True
+
+    particle_1 = Particle(
+        mass=1.0, position=np.array([1, 2, 3]), velocity=np.array([2, 3, 4])
+    )
+
+    particle_1_copy = Particle(
+        mass=1.0, position=np.array([1, 2, 3]), velocity=np.array([2, 3, 4])
+    )
+
+    assert hash(particle_1) == hash(particle_1_copy)
+    assert particle_1.mass == 1.0
+
+
+def test_particle_index_error():
+
+    with pytest.raises(Exception) as exception_info:
+
+        particle_3 = Particle(
+            mass=1.0, position=np.array([1, 2]), velocity=np.array([1, 2, 3])
+        )
+
+    assert (
+        exception_info.value.args[0]
+        == "The position vector must be a numpy array of length 3."
+    )
+    assert not particle_3
