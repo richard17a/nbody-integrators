3f8df4c152662cf39c4a31acf05bebd839080cce
Richard Anslow <r.anslow@outlook.com>
2022-06-04 15:43:08 +0100

    updated file structure and added requirement files for pytest setup

---
+++ tests/test_particle.py
@@ -0,0 +1,18 @@
+from nbody.particle import Particle
+import numpy as np
+
+
+def test_particle():
+    particle_1 = Particle(
+        mass=1.0,
+        position=np.array([1, 2, 3]),
+        velocity=np.array([1, 2, 3]),
+    )
+
+    particle_1_copy = Particle(
+        mass=1.0,
+        position=np.array([1, 2, 3]),
+        velocity=np.array([1, 2, 3]),
+    )
+
+    assert particle_1 and particle_1_copy
