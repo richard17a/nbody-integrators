c6194763e8c7c2dbe30b6078c4e5f2ab80afbb70
Richard Anslow <r.anslow@outlook.com>
2022-06-17 16:30:14 +0100

    added tests for position and velocity property setter methods

--- tests/test_particle.py
+++ tests/test_particle.py
@@ -1,5 +1,6 @@
 from nbody.particle import Particle
 import numpy as np
+import pytest
 
 
 def test_particle():
@@ -24,3 +25,31 @@ def test_particle():
         and particle_1_copy.z_pos == 3
     )
     assert list(particle_1.velocity) == [1, 2, 3]
+
+
+def test_particle_position_setter():
+    particle_1 = Particle(
+        mass=1.0, position=np.array([1, 2, 3]), velocity=np.array([1, 2, 3])
+    )
+
+    particle_1.position = np.array([2, 3, 4])
+
+    with pytest.raises(TypeError):
+        particle_1.position = [1, 2, 3]
+
+    with pytest.raises(TypeError):
+        particle_1.position = np.array([1, 2])
+
+
+def test_particle_velocity_setter():
+    particle_1 = Particle(
+        mass=1.0, position=np.array([1, 2, 3]), velocity=np.array([1, 2, 3])
+    )
+
+    particle_1.velocity = np.array([2, 3, 4])
+
+    with pytest.raises(TypeError):
+        particle_1.velocity = [1, 2, 3]
+
+    with pytest.raises(TypeError):
+        particle_1.velocity = np.array([1, 2])
--- tests/test_particles.py
+++ tests/test_particles.py
@@ -63,3 +63,51 @@ def test_remove_particle():
 
     with pytest.raises(TypeError):
         particles.remove_particle(particle_1)
+
+
+def test_particles_positions_setter():
+    particle_1 = Particle(
+        mass=1.0,
+        position=np.array([1, 2, 3]),
+        velocity=np.array([1, 2, 3]),
+    )
+
+    particle_2 = Particle(
+        mass=2.0,
+        position=np.array([-1, -2, -3]),
+        velocity=np.array([-1, -2, -3]),
+    )
+
+    particles = Particles()
+    particles.add_particle(particle_1)
+    particles.add_particle(particle_2)
+
+    with pytest.raises(TypeError):
+        particles.positions = [1, 2, 3, -1, -2, -3]
+
+    with pytest.raises(ValueError):
+        particles.positions = np.array([1, 2, 3])
+
+
+def test_particles_velocities_setter():
+    particle_1 = Particle(
+        mass=1.0,
+        position=np.array([1, 2, 3]),
+        velocity=np.array([1, 2, 3]),
+    )
+
+    particle_2 = Particle(
+        mass=2.0,
+        position=np.array([-1, -2, -3]),
+        velocity=np.array([-1, -2, -3]),
+    )
+
+    particles = Particles()
+    particles.add_particle(particle_1)
+    particles.add_particle(particle_2)
+
+    with pytest.raises(TypeError):
+        particles.velocities = [1, 2, 3, -1, -2, -3]
+
+    with pytest.raises(ValueError):
+        particles.velocities = np.array([1, 2, 3])
